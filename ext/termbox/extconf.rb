39bcde43ea8eb4e4996795f4dd0849d318ddcc6c
Jonathan Garay <jonathan@devmask.net>
2013-11-09 14:36:19 -0600

    added extconf

--- ext/termbox/extconf.rb
+++ ext/termbox/extconf.rb
@@ -3,6 +3,5 @@ extension_name = 'termbox'
 
 have_header('termbox.h')
 create_header
-
 dir_config(extension_name)
 create_makefile(extension_name)
--- lib/termbox.rb
+++ lib/termbox.rb
@@ -1,4 +1,5 @@
 require "termbox/version"
+require File.expand_path('../termbox', __FILE__)
 
 module Termbox
 
